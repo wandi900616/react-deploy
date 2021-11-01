c2e4a64feba0f18cd55df9feb949e2f5e9a19c9b
iswandi <ease.ja@gmail.com>
2021-11-02 07:38:59 +0800

    Updates

---
+++ precache-manifest.67f63cc73eda9e0ba694275dc0e8d325.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697d48a9c64c2e4a9afd5dcef2896c00",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "54969959f91d40309658",
+    "url": "/react-deploy/static/css/main.a25574c1.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/react-deploy/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54969959f91d40309658",
+    "url": "/react-deploy/static/js/main.92679194.chunk.js"
+  },
+  {
+    "revision": "cb3e81b42833b05aaaf6",
+    "url": "/react-deploy/static/js/runtime-main.028a3edb.js"
+  }
+]);
